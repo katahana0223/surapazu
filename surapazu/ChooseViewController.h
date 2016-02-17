38827db0fc7fec1fce21a90226fed3920a880e9b
津川 <hanahanamocomoco@softbank.ne.jp>
2016-02-17 20:48:26 +0900

    20160217

---
+++ surapazu/ChooseViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChooseViewController.h
+//  surapazu
+//
+//  Created by 津川 on 2016/02/17.
+//  Copyright (c) 2016年 津川. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChooseViewController : UIViewController
+
+@end
---
+++ surapazu/firstViewController.h
@@ -0,0 +1,13 @@
+//
+//  firstViewController.h
+//  surapazu
+//
+//  Created by 津川 on 2016/02/10.
+//  Copyright (c) 2016年 津川. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface firstViewController : UIViewController
+
+@end
