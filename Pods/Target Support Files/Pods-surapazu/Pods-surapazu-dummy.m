f1304d0b72dcaca1e13d943cf232d3bff060dde9
aminaura <aura.otsuka@gmail.com>
2016-04-03 14:27:31 +0900

    add UnlockSlider

---
+++ Pods/Target Support Files/Pods-surapazu/Pods-surapazu-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_surapazu : NSObject
+@end
+@implementation PodsDummy_Pods_surapazu
+@end
---
+++ surapazu/StartViewController.h
@@ -0,0 +1,17 @@
+//
+//  UIViewController+StartViewController.h
+//  surapazu
+//
+//  Created by AuraOtsuka on 2016/04/03.
+//  Copyright © 2016年 AuraOtsuka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartViewController : UIViewController{
+
+    
+}
+
+
+@end
